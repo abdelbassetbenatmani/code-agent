ff00b30a00c5d1de9d6f19edb1da456667f8b159
abdelbassetbenatmani <abdelbassetbenatmani@gmail.com>
2025-07-24 14:04:13 +0100

    feat: enhance SidebarNav with dynamic URL handling and state management

--- components/Dashboard/Profile/SidebarNav.tsx
+++ components/Dashboard/Profile/SidebarNav.tsx
@@ -1,28 +1,34 @@
 import React from "react";
-
 import { User, Palette, SlidersHorizontal, Users } from "lucide-react";
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
+
 const sidebarNavItems = [
   {
     title: "Profile",
     icon: <User size={18} />,
     href: "/settings",
+    param: "profile",
   },
   {
     title: "Account",
     icon: <SlidersHorizontal size={18} />,
     href: "/settings/account",
+    param: "account",
   },
   {
     title: "Teams",
     icon: <Users size={18} />,
     href: "/settings/teams",
+    param: "teams",
   },
   {
     title: "Appearance",
     icon: <Palette size={18} />,
     href: "/settings/appearance",
+    param: "appearance",
   },
 ];
+
 const SidebarNav = ({
   activeTab,
   setActiveTab,
@@ -32,9 +38,30 @@ const SidebarNav = ({
     React.SetStateAction<"profile" | "account" | "appearance" | "teams">
   >;
 }) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const handleNavClick = (
+    tab: "profile" | "account" | "appearance" | "teams"
+  ) => {
+    // Update component state
+    setActiveTab(tab);
+
+    // Create a new URLSearchParams object based on the current params
+    const params = new URLSearchParams(searchParams);
+
+    // Set the tab parameter
+    params.set("tab", tab);
+
+    // Update the URL without refreshing the page
+    const newUrl = `${pathname}?${params.toString()}`;
+    router.push(newUrl, { scroll: false });
+  };
+
   return (
     <nav className="space-y-1">
-      {sidebarNavItems.map((item: any) => (
+      {sidebarNavItems.map((item) => (
         <button
           key={item.title}
           className={`flex cursor-pointer items-center p-2 text-sm font-medium transition-colors duration-200 ease-in-out group rounded-md focus:ring-0
@@ -45,7 +72,7 @@ const SidebarNav = ({
             }
           `}
           onClick={() =>
-            setActiveTab(
+            handleNavClick(
               item.title.toLowerCase() as
                 | "profile"
                 | "account"
